9ff81b08bc60c79c7ef19659d2004d37d7aae2f1
Cedric <cmi@voxteneo.com>
2017-07-01 21:59:29 +0200

    add unit test

---
+++ ExtensionDemo/Serialisation.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Serialisation.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Serialisation.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c62d0791-6aa2-408c-a403-9019e71c9c3b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ExtensionDemo/Serialisation.Test/XmlSerialisationTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Serialisation.Test
+{
+    [TestClass]
+    public class XmlSerialisationTest
+    {
+        [TestMethod]
+        public void XmlSerialisationNull()
+        {
+            object o = null;
+            Assert.AreEqual(string.Empty, o.Serialize());
+        }
+
+        [TestMethod]
+        public void XmlSerialisationString()
+        {
+            string test = "test";
+            string result = test.Serialize();
+            string expectedXml = "<?xml version=\"1.0\" encoding=\"utf-16\"?>\r\n<string>test</string>";
+            Assert.AreEqual(expectedXml, result);
+        }
+    }
+}
